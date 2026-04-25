00a78f7374255fb62565639bce612abd122be6ba
Thabiso Kubeka <65237633+Thabiso3028@users.noreply.github.com>
2026-04-25 07:42:58 +0000

    Checkout Test

---
+++ SauceDemoTests/Tests/CheckoutTests.cs
@@ -0,0 +1,34 @@
+using SauceDemoTests.Pages;
+
+namespace SauceDemoTests.Tests;
+
+public class CheckoutTests : BaseTest
+{
+
+    public async Task User_Can_Complete_Checkout_Flow()
+    {
+        var loginPage = new LoginPage(Page);
+        var inventoryPage = new InventoryPage(Page);
+
+        await loginPage.GotoAsync();
+        await loginPage.LoginAsync("standard_user", "secret_sauce");
+
+        await inventoryPage.ExpectOnInventoryPage();
+        await inventoryPage.AddItemToCartAsync("Sauce Labs Backpack");
+        await inventoryPage.AddItemToCartAsync("Sauce Labs Bike Light");
+
+        Assert.That(await inventoryPage.GetCartCountAsync(), Is.EqualTo(2));
+
+        await inventoryPage.GoToCartAsync();
+        await Expect(Page).ToHaveURLAsync("**/cart.html");
+        await Expect(Page.GetByText("Sauce Labs Backpack")).ToBeVisibleAsync();
+    }
+
+    public async Task Locked_User_Shows_Error()
+    {
+        var loginPage = new LoginPage(Page);
+        await loginPage.GotoAsync();
+        await loginPage.LoginAsync("locked_out_user", "secret_sauce");
+        await Expect(Page.GetByText("Sorry, this user has been locked out.")).ToBeVisibleAsync();
+    }
+}
