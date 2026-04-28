442af8f6e66dbfaabf978a38a52b8e16f056b987
Thabiso Kubeka <65237633+Thabiso3028@users.noreply.github.com>
2026-04-28 13:49:57 +0000

    Added API test file

---
+++ SauceDemoTests/API/ApiTests.cs
@@ -0,0 +1,21 @@
+/*using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace SauceDemoTests.API;
+
+public class ApiTests : PlaywrightTest
+{
+    private IAPIRequestContext _apiContext;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        _apiContext = await Playwright.APIRequest.NewContextAsync(new()
+        {
+            BaseURL = "https://reqres.in/api"
+        });
+    }
+
+}*/
